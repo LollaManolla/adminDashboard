d18850eb9c98f0ef85678e980432448415ff182a
LollaManolla <lollamanolla@gmail.com>
2017-05-28 18:08:24 +0200

    product updated

---
+++ js/sales.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  //for the scrollbar
+  //change browser scrollbar
+  // no need to modify this
+  $(".fullPageScroll").mCustomScrollbar({
+    axis:"yx",
+    theme:"3d-thick"
+  });
+
+  //for the forms
+  // change look of the select forms
+  // no need to modify this
+    $('select').niceSelect();
+
+
+
+});
+
+//initiate calendar, clicking on icon
+$( function() {
+  $( ".datepicker" ).datepicker({
+    showOn: "button",
+    buttonImage: "./_common/images/calendar.png",
+    buttonImageOnly: true,
+    buttonText: "Select date"
+  });
+} );

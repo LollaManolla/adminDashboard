196a66007ffdb5af22331500e2bf89e86a05dc67
Marija Jovicevic <marija.jovicevic@diwanee.com>
2017-06-01 12:45:29 +0200

    Other request fix
    Scroll added

--- js/otherRequests.js
+++ js/otherRequests.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-  $( ".add, .decline" ).hover(
-  function() {
-    $( this ).addClass( "hover" );
-  }, function() {
-    $( this ).removeClass( "hover" );
-  }
-);
+
+    //show hover notification
+    $( ".add, .decline" ).hover(
+        function() {
+            $( this ).addClass( "hover" );
+        }, function() {
+            $( this ).removeClass( "hover" );
+        }
+    );
+
+    //nice scroll
+    $(".scrollTop, .scrollBottom").mCustomScrollbar({
+        theme:"3d-thick"
+    });
+
 });

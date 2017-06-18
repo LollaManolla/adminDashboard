e2d60f76aba1f674ba05be51939a425763ab0388
LollaManolla <lollamanolla@gmail.com>
2017-06-18 15:59:10 +0200

    enquiry

--- js/products-enquiryPrice.js
+++ js/products-enquiryPrice.js
@@ -1,9 +1,5 @@
 
 $(document).ready(function () {
-
-
-
-
   //for the scrollbar
   //change browser scrollbar
   // no need to modify this
@@ -11,14 +7,28 @@ $(document).ready(function () {
     theme:"3d-thick"
   });
 
-  //open modal window
-  $(".c-modal-open").click(function(e) {
-      $('.c-modal.c-modal--form').hide();
-      $('.c-modal.c-modal--form').slideDown();
-  });
-  $(".c-modal__btn-close").click(function() {
-    $('.c-modal.c-modal--form').slideUp();
+
+  //slide down info
+  $(".enquiry-message").click(function () {
+    var $wrap = $(this).parents('.b-list-enquiry__details');
+
+    if ($wrap.find('.b-list-enquiry__full').is(':visible')) {
+      $(".b-list-enquiry__full").slideUp(300);
+    }
+    else {
+      $('.b-list-enquiry__full').slideUp(300);
+      $wrap.find('.b-list-enquiry__full').stop();
+      $wrap.find('.b-list-enquiry__full').slideDown(300);
+    }
   });
 
+  //open modal window
+$(".c-modal-open").click(function(e) {
+    $('.c-modal.c-modal--form').hide();
+    $('.c-modal.c-modal--form').slideDown();
+});
+$(".c-modal__btn-close").click(function() {
+  $('.c-modal.c-modal--form').slideUp();
+});
 
 });

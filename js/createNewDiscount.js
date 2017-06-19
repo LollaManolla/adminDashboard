c514dafdf0d6667d77c3ad078045b96d6793a936
LollaManolla <lollamanolla@gmail.com>
2017-06-19 20:59:46 +0200

    new discount

--- js/createNewDiscount.js
+++ js/createNewDiscount.js
@@ -1,6 +1,18 @@
 //events JS
 $(document).ready(function () {
 
+  //for the scrollbar
+  //change browser scrollbar
+  // no need to modify this
+  $(".fullPageScroll").mCustomScrollbar({
+    axis:"yx",
+    theme:"3d-thick"
+  });
+
+  $(".scrollSelect").mCustomScrollbar({
+    theme:"3d-thick"
+  });
+
   //for the forms
   // change look of the select forms
   // no need to modify this
